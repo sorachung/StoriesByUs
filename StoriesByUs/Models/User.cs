17b4b6ec8409a65fc28128cb40c64a9fadff2391
sorachung <sorachung.src@gmail.com>
2022-04-28 09:22:54 -0500

    added User model

---
+++ StoriesByUs/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoriesByUs.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        public string Bio { get; set; }
+
+        [Required]
+        public int UserTypeId { get; set; }
+    }
+}
