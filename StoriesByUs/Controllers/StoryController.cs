38dcc73448c1c7eeb0db53e490d9d82b6367e2db
sorachung <sorachung.src@gmail.com>
2022-05-02 10:47:36 -0500

    created StoryController and added Get method

---
+++ StoriesByUs/Controllers/StoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoriesByUs.Models;
+using StoriesByUs.Repositories;
+
+namespace StoriesByUs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoryController : ControllerBase
+    {
+        private readonly IStoryRepository _storyRepository;
+        public StoryController(IStoryRepository storyRepository)
+        {
+            _storyRepository = storyRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_storyRepository.GetAll());
+        }
+    }
+}
