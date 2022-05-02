c3919b182b4369e636a0b226fabc218df85ab05a
sorachung <sorachung.src@gmail.com>
2022-05-02 15:26:08 -0500

    added Browse by Genre section in Browse.js and added state for genres

--- StoriesByUs/client/src/components/browse/Browse.js
+++ StoriesByUs/client/src/components/browse/Browse.js
@@ -1,13 +1,32 @@
-import { Container, Link } from "@mui/material";
-import { Link as RouterLink, MemoryRouter } from "react-router-dom";
-import React from "react";
+import { Container, Link, Stack, Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import React, { useState } from "react";
 export default function Browse() {
+  const [genres, setGenres] = useState([]);
+
   return (
     <Container maxWidth="xl">
       <h1>Browse Stories</h1>
-      <Link component={RouterLink} to="/browse/all">
-        Browse all
-      </Link>
+      <Stack spacing={2}>
+        <Link component={RouterLink} to="/browse/all">
+          <h2>Browse all</h2>
+        </Link>
+        <Box component="section">
+          <h2>Browse by Genre</h2>
+          {genres.map((g, i) => (
+            <span key={g.id}>
+              {i > 0 && ", "}
+              <Link
+                component={RouterLink}
+                color="primary"
+                to={`/browse/genre/${g.id}`}
+              >
+                {g.name}
+              </Link>
+            </span>
+          ))}
+        </Box>
+      </Stack>
     </Container>
   );
 }
