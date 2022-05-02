5abd539f4ec512d70bf6c5ab29c7f859487283a0
sorachung <sorachung.src@gmail.com>
2022-05-02 14:53:33 -0500

    added tags, genres, and other story info into story card; changed Links to appropriate ones

--- StoriesByUs/client/src/components/stories/StoryCard.js
+++ StoriesByUs/client/src/components/stories/StoryCard.js
@@ -2,10 +2,7 @@ import React from "react";
 import {
   Card,
   CardHeader,
-  CardActions,
   CardContent,
-  Button,
-  Typography,
   Divider,
   Link,
   Stack,
@@ -19,11 +16,19 @@ export default function StoryCard({ story }) {
       <CardHeader
         title={
           <>
-            <Link component={RouterLink} color="primary" to="/">
+            <Link
+              component={RouterLink}
+              color="primary"
+              to={`/works/${story.id}/chapters/1`}
+            >
               {story.title}
             </Link>
             <span> by </span>
-            <Link component={RouterLink} color="primary" to="/">
+            <Link
+              component={RouterLink}
+              color="primary"
+              to={`/users/${story.user.id}`}
+            >
               {story.user.displayName}
             </Link>
           </>
@@ -31,11 +36,45 @@ export default function StoryCard({ story }) {
       />
       <CardContent>
         <Stack spacing={2}>
-          <Box component="ul">Genres</Box>
-          <Box component="ul">Tags</Box>
+          <Box component="div">
+            <div>
+              Genres:{" "}
+              {story.genres.map((g, i) => (
+                <span key={g.id}>
+                  {i > 0 && ", "}
+                  <Link
+                    component={RouterLink}
+                    color="primary"
+                    to={`/browse/genre/${g.id}`}
+                  >
+                    {g.name}
+                  </Link>
+                </span>
+              ))}
+            </div>
+          </Box>
+          <Box component="div">
+            <div>
+              Tags:{" "}
+              {story.tags.map((t, i) => (
+                <span key={t.id}>
+                  {i > 0 && ", "}
+                  <Link
+                    component={RouterLink}
+                    color="primary"
+                    to={`/browse/tag/${t.id}`}
+                  >
+                    {t.name}
+                  </Link>
+                </span>
+              ))}
+            </div>
+          </Box>
           <Box component="div">{story.summary}</Box>
           <Box component="footer" sx={{ display: "flex" }}>
             <span>Rating: {story.rating.level}</span>
+            <Divider orientation="vertical" flexItem sx={{ m: "0 0.5em" }} />
+            <span>Chapters: {story.chapters.length}</span>
             <Divider orientation="vertical" flexItem sx={{ m: "0 0.5em" }} />
             <span>
               Updated:{" "}
@@ -48,6 +87,17 @@ export default function StoryCard({ story }) {
             </span>
             <Divider orientation="vertical" flexItem sx={{ m: "0 0.5em" }} />
             <span>Complete: {story.complete ? "Yes" : "No"}</span>
+            <Divider orientation="vertical" flexItem sx={{ m: "0 0.5em" }} />
+            <span>
+              Bookmarks:{" "}
+              <Link
+                component={RouterLink}
+                color="primary"
+                to={`/works/${story.id}/bookmarks/`}
+              >
+                {story.bookmarks.length}
+              </Link>
+            </span>
           </Box>
         </Stack>
       </CardContent>
