6cbcadd2d4a93935e69a281deb7fa366d7eaca4b
sorachung <sorachung.src@gmail.com>
2022-05-02 10:26:22 -0500

    added Rating and Story models

---
+++ StoriesByUs/Models/Story.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StoriesByUs.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Notes{ get; set; }
+        public DateTime PublishedDateTime { get; set; }
+        public DateTime LastUpdatedDateTime { get; set; }
+        public bool Complete { get; set; }
+        public Rating Rating { get; set; }
+        public User User { get; set; }
+    }
+}
