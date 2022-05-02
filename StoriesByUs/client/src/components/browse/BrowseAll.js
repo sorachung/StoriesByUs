b34591d68686284c6dd108bbcc9fe946b59a6242
sorachung <sorachung.src@gmail.com>
2022-05-02 11:16:12 -0500

    created state and useEffect for stories

--- StoriesByUs/client/src/components/browse/BrowseAll.js
+++ StoriesByUs/client/src/components/browse/BrowseAll.js
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { getAllStories } from "../../modules/storyManager";
 import { Container, Link } from "@mui/material";
 import { Link as RouterLink, MemoryRouter } from "react-router-dom";
-import React from "react";
+
 export default function BrowseAll() {
+  const [stories, setStories] = useState([]);
+
+  const getStories = () => {
+    getAllStories().then((storiesArr) => setStories(storiesArr));
+  };
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
   return (
     <Container maxWidth="xl">
       <h1>Browse: All</h1>
+      {stories.map((story) => story.title)}
     </Container>
   );
 }
