d4ea225389647b90bad8432aa431b34cb942f0cf
sorachung <sorachung.src@gmail.com>
2022-05-02 12:46:37 -0500

    created/updated Bookmark and Genre models

---
+++ StoriesByUs/Models/Bookmark.cs
@@ -0,0 +1,11 @@
+﻿namespace StoriesByUs.Models
+{
+    public class Bookmark
+    {
+        public int Id { get; set; }
+        public int StoryId { get; set; }
+        public int UserId { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
