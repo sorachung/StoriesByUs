b043e418a65f52e045b1005509c2f56f8f57dd94
sorachung <sorachung.src@gmail.com>
2022-05-02 12:44:46 -0500

    created chapter, genre, and tag models

---
+++ StoriesByUs/Models/Chapter.cs
@@ -0,0 +1,13 @@
+﻿namespace StoriesByUs.Models
+{
+    public class Chapter
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int PlaceInOrder { get; set; }
+        public string Notes { get; set; }
+        public Story Story { get; set; }
+
+    }
+}
