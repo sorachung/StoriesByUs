50c021f551eba67f71850fea9b5682fa8adb9c07
sorachung <sorachung.src@gmail.com>
2022-05-03 09:21:29 -0500

    made genre controller with two get actions

---
+++ StoriesByUs/Controllers/GenreController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoriesByUs.Models;
+using StoriesByUs.Repositories;
+namespace StoriesByUs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly IGenreRepository _genreRepository;
+        public GenreController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_genreRepository.GetAll());
+        }
+
+        [HttpGet("withStoryCount")]
+        public IActionResult GetWithStoryCount()
+        {
+            return Ok(_genreRepository.GetAllWithStoryCountOverZero());
+        }
+    }
+}
