cc5761891deca0d418a359ecdb492114b5fd8c28
sorachung <sorachung.src@gmail.com>
2022-05-04 10:53:10 -0500

    implemented chapter navigation

--- StoriesByUs/client/src/components/chapters/Chapter.js
+++ StoriesByUs/client/src/components/chapters/Chapter.js
@@ -5,6 +5,11 @@ import {
   Box,
   Typography,
   Divider,
+  Button,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
 } from "@mui/material";
 import { Link as RouterLink, useParams, useHistory } from "react-router-dom";
 import React, { useEffect, useState } from "react";
@@ -19,6 +24,7 @@ export default function Chapter() {
   const { placeInOrder } = useParams();
   const { storyId } = useParams();
   const history = useHistory();
+  const [chosenChapterNumber, setChosenChapterNumber] = useState(placeInOrder);
 
   const getAndSetChapter = () => {
     getChapter(placeInOrder, storyId).then((chapterData) => {
@@ -41,15 +47,61 @@ export default function Chapter() {
   useEffect(() => {
     getAndSetChapter();
     getAndSetStory();
+    window.scrollTo(0, 0);
   }, [placeInOrder, storyId]);
+
+  if (!story.chapters || !story.user) {
+    return null;
+  }
 
   return (
     <>
       <Container maxWidth="lg">
-        <Box component="section">
-          <ChapterStoryInfo story={story} />
-        </Box>
         <Stack spacing={2} divider={<Divider flexItem />}>
+          <Box component="header">
+            {chapter.placeInOrder === 1 ? (
+              ""
+            ) : (
+              <Link
+                component={RouterLink}
+                to={`/works/${storyId}/chapters/${chapter.placeInOrder - 1}`}
+              >
+                <Button variant="contained">Previous Chapter</Button>
+              </Link>
+            )}
+            {story.chapters.length === 1 ? (
+              ""
+            ) : (
+              <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+                <Select
+                  id="demo-simple-select"
+                  value={placeInOrder}
+                  onChange={(event) =>
+                    history.push(
+                      `/works/${storyId}/chapters/${event.target.value}`
+                    )
+                  }
+                >
+                  {story.chapters.map((c) => (
+                    <MenuItem value={c.placeInOrder}>{c.title}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+            {chapter.placeInOrder === story.chapters?.length ? (
+              ""
+            ) : (
+              <Link
+                component={RouterLink}
+                to={`/works/${storyId}/chapters/${chapter.placeInOrder + 1}`}
+              >
+                <Button variant="contained">Next Chapter</Button>
+              </Link>
+            )}
+          </Box>
+          <Box component="section">
+            <ChapterStoryInfo story={story} />
+          </Box>
           <Box component="section">
             <Typography variant="h4" align="center" mt="0.5em">
               {story.title}
@@ -77,6 +129,28 @@ export default function Chapter() {
                 __html: DOMPurify.sanitize(chapter.body),
               }}
             ></Typography>
+          </Box>
+          <Box component="footer">
+            {chapter.placeInOrder === 1 ? (
+              ""
+            ) : (
+              <Link
+                component={RouterLink}
+                to={`/works/${storyId}/chapters/${chapter.placeInOrder - 1}`}
+              >
+                <Button variant="contained">Previous Chapter</Button>
+              </Link>
+            )}
+            {chapter.placeInOrder === story.chapters?.length ? (
+              ""
+            ) : (
+              <Link
+                component={RouterLink}
+                to={`/works/${storyId}/chapters/${chapter.placeInOrder + 1}`}
+              >
+                <Button variant="contained">Next Chapter</Button>
+              </Link>
+            )}
           </Box>
         </Stack>
       </Container>
