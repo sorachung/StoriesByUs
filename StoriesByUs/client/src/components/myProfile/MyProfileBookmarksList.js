da23343fb581842e3672a1807dd4b18154a2e31b
sorachung <sorachung.src@gmail.com>
2022-05-06 14:08:54 -0500

    added editing/deleting bookmarks from my profile page

--- StoriesByUs/client/src/components/myProfile/MyProfileBookmarksList.js
+++ StoriesByUs/client/src/components/myProfile/MyProfileBookmarksList.js
@@ -1,15 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { Container, Box, Card, CardHeader, CardContent } from "@mui/material";
+import {
+  Container,
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 import StoryCard from "../stories/StoryCard";
-import { getBookmarksByUser } from "../../modules/bookmarkManager";
+import {
+  deleteBookmark,
+  getBookmarksByUser,
+  putBookmark,
+} from "../../modules/bookmarkManager";
 
 export default function MyProfileBookmarksList({ user }) {
   const [bookmarks, setBookmarks] = useState([]);
+  const [chosenBookmark, setChosenBookmark] = useState({});
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const getBookmarks = () => {
     getBookmarksByUser(user.id).then((bookmarksData) =>
       setBookmarks(bookmarksData)
     );
+  };
+
+  const editBookmark = () => {
+    putBookmark(chosenBookmark).then(getBookmarks);
+  };
+
+  const removeBookmark = () => {
+    deleteBookmark(chosenBookmark.id).then(getBookmarks);
   };
 
   useEffect(() => {
@@ -25,10 +61,62 @@ export default function MyProfileBookmarksList({ user }) {
             <Card variant="outlined" sx={{ minWidth: 275 }}>
               <CardHeader title="Notes: " />
               <CardContent>{bookmark.notes}</CardContent>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setChosenBookmark(bookmark);
+                    handleClickOpen();
+                  }}
+                >
+                  Edit
+                </Button>
+              </CardActions>
             </Card>
           </Box>
         </Box>
       ))}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Bookmark</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="notes-field"
+            label="Notes"
+            type="text"
+            fullWidth
+            multiline
+            rows={4}
+            variant="outlined"
+            value={chosenBookmark.notes}
+            onChange={(evt) => {
+              const copy = { ...chosenBookmark };
+              copy.notes = evt.target.value;
+              setChosenBookmark(copy);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              removeBookmark();
+              handleClose();
+            }}
+          >
+            Delete
+          </Button>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            onClick={() => {
+              editBookmark();
+              handleClose();
+            }}
+          >
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
